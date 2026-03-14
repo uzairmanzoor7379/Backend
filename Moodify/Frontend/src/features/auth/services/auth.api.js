86fadce7b08aa1cc0d6dfac954fd91e7f287ed77
Uzair Khan <uk873195@gmail.com>
2026-03-14 21:51:48 +0530

    second

--- Moodify/Frontend/src/features/auth/services/auth.api.js
+++ Moodify/Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://backend-c0ap.onrender.com",
     withCredentials: true
 })
  export async function register(username , email , password) {

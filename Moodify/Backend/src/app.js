492e6ec2faa4fd701ea649ba9c4736a4234aedcd
Uzair Khan <uk873195@gmail.com>
2026-03-14 22:44:22 +0530

    third

--- Moodify/Backend/src/app.js
+++ Moodify/Backend/src/app.js
@@ -3,17 +3,27 @@ const cookie = require('cookie-parser');
 const AuthRouter = require('./routes/auth.route');
 const cors = require('cors');
 const songRouter = require('./routes/song.route');
+const path = require('path');
+
 const app = express()
+
 app.use(express.json())
 app.use(cookie())
-app.use(express.static('./public'))
 app.use(cors({
-    credentials : true,
-    origin : 'http://localhost:5173'
+    credentials: true,
+    origin: ['http://localhost:5173', 'https://backend-c0ap.onrender.com']
 }))
 
-// Routes
+// API Routes pehle
 app.use('/api/auth', AuthRouter)
-app.use('/api/songs',songRouter)
+app.use('/api/songs', songRouter)
+
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'))
+})
 
 module.exports = app

03d7820f86353362297e5010a85bbd0da49357c4
Uzair Khan <uk873195@gmail.com>
2026-03-14 23:15:31 +0530

    fifth

--- Moodify/Backend/src/app.js
+++ Moodify/Backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cookie())
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'https://backend-c0ap.onrender.com']
+    origin: ['http://localhost:5173', 'https://backend-gac6.onrender.com']
 }))
 
 // API Routes pehle
--- Moodify/Frontend/src/features/home/services/songs.api.js
+++ Moodify/Frontend/src/features/home/services/songs.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "https://backend-c0ap.onrender.com",
+    baseURL: "https://backend-gac6.onrender.com",
     withCredentials : true
 })
 
